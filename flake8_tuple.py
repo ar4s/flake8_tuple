5c91ea1f48b38e4f6f99345a699229741354bf77
Arkadiusz Adamski <arkadiusz.adamski@gmail.com>
2015-08-23 13:43:58 +0200

    better checker

--- flake8_tuple.py
+++ flake8_tuple.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 import ast
+import token
+import tokenize
+
+
+ERROR_CODE = 'T801'
+ERROR_MESSAGE = 'one element tuple',
 
 
 class TupleChecker(object):
@@ -12,13 +18,64 @@ class TupleChecker(object):
         self.tree = tree
 
     def run(self):
-        for stmt in ast.walk(self.tree):
-            if not isinstance(stmt, ast.Tuple):
-                continue
-            if len(stmt.elts) == 1:
-                yield (
-                    stmt.lineno,
-                    stmt.col_offset,
-                    'T801 one element tuple',
-                    'T801'
+        code = ''
+        with open(self.filename, 'r') as f:
+            lines = f.readlines()
+            noqa = get_noqa_lines(lines)
+            code = '\n'.join(lines)
+
+        for error in check_for_wrong_tuple(self.tree, code, noqa):
+            yield (
+                error[0],
+                error[1],
+                ('{}: {}').format(ERROR_CODE, ERROR_MESSAGE),
+                type(self)
+            )
+
+
+def get_noqa_lines(code):
+    tokens = tokenize.generate_tokens(lambda L=iter(code): next(L))
+    noqa = [
+        token[2][0]
+        for token in tokens
+        if (
+            token[0] == tokenize.COMMENT and
+            (
+                token[1].endswith('noqa') or
+                (
+                    isinstance(token[0], str) and token[0].endswith('noqa')
                 )
+            )
+        )]
+    return noqa
+
+
+def check_code_for_wrong_tuple(code):
+    tree = ast.parse(code)
+    noqa = get_noqa_lines(code.split("\n"))
+    return check_for_wrong_tuple(tree, code, noqa)
+
+
+def check_for_wrong_tuple(tree, code, noqa):
+    errors = []
+    candidates = []
+    for assign in ast.walk(tree):
+        if not isinstance(assign, ast.Assign) or assign.lineno in noqa:
+            continue
+        for tuple_el in ast.walk(assign):
+            if isinstance(tuple_el, ast.Tuple) and len(tuple_el.elts) == 1:
+                candidates.append((assign.lineno, assign.col_offset))
+                break
+    if not candidates:
+        return []
+    for candidate in candidates:
+        tokens = tokenize.generate_tokens(lambda L=iter(code.split("\n")): next(L))
+        for t in tokens:
+            if t.start[0] == candidate[0] and t.start[1] >= candidate[1]:
+                while t.type != token.OP and t.string != '=':
+                    t = next(tokens)
+                t = next(tokens)
+                if t.type != token.OP and t.string != '(' and t.type != token.ENDMARKER:
+                    errors.append(t.start)
+                    t = next(tokens)
+    return errors
--- tests/test_pep8_tuple.py
+++ tests/test_pep8_tuple.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
-import ast
 import unittest
 
 from ddt import ddt, data, unpack
 
-from flake8_tuple import TupleChecker
-
-
-def _create_checker(code):
-    return TupleChecker(ast.parse(code), 'test.py')
+from flake8_tuple import check_code_for_wrong_tuple
 
 
 @ddt
 class Testflake8Tuple(unittest.TestCase):
     @unpack
     @data(
-        ("foo = 1,", 1),
+        ("bar = 1,  # noqa", 0),
+        ("bar = 1, 2", 0),
         ("foo = 1", 0),
+        ("foo = (1,)", 0),
+        ("foo = 1,", 1),
         ("bar = 1; foo = bar,", 1),
-        ("foo = (\n3,\n4,\n)", 0),
+        ("foo = (\n3,\n4,\n)\nbar = 10,", 1),
+        ("foo = 3,\nbar = 10,\nfoo_bar = 2,", 3),
+        ("foo = 3,\nbar = 10 \nfoo_bar = 2,", 2),
+        ("foo = 3,\nbar = 10,\nfoo_bar = 2",  2),  # noqa
+        ("foo = 3 \nbar = 10 \nfoo_bar = 2,", 1),
+        ("foo = 3 \nbar = 10 \nfoo_bar = 2",  0),  # noqa
     )
     def test_tuple(self, code, errors):
-        checker = _create_checker(code)
-        self.assertEqual(len(list(checker.run())), errors)
+        result = check_code_for_wrong_tuple(code)
+        self.assertEqual(len(result), errors)
